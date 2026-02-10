cfc8add86fc14133b295d22a7764a34e05e8a7db
wuyao <wuyaocheng0511@gmail.com>
2026-02-10 11:42:36 +0800

    finsh1

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def home():
+    # 這裡的 user_name 會傳送到 HTML 的 {{ user_name }} 位置
+    return render_template('index.html', user_name="Sullyoon")
 
 if __name__ == '__main__':
-    app.run()
+    # host='0.0.0.0' 代表監聽所有網路介面
+    app.run(debug=True, host='0.0.0.0', port=5000)
